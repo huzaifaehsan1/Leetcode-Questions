c703e39425102a1f0ce409f918c6d05357b7330f
Huzaifa Ehsan <56621886+huzaifaehsan1@users.noreply.github.com>
2021-12-21 17:09:51 -0600

    Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+class TwoSum{
+  public static void main(String[] args){
+
+  }
+
+
+  public int[] twoSum(int[] numbers, int target) {
+    int[] result = new int[2];
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < numbers.length; i++) {
+        if (map.containsKey(target - numbers[i])) {
+            result[1] = i;
+            result[0] = map.get(target - numbers[i]);
+            return result;
+        }
+        map.put(numbers[i], i);
+    }
+    return result;
+  }
+}
